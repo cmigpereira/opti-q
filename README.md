90c22055bcaad6d0d1722cc6165d4e492ce854af
Carlos Pereira <karlospereira@gmail.com>
2023-10-31 23:56:21 +0000

    Added files for Opti-Q app
    Added first version of the files for the Opti-Q streamlit app

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# opti-q
-Natural language query bot for your ODP Snowflake database
+# Opti-Q
+ 
+This is a Streamlit app that allows users to ask natural language questions to an Azure OpenAI LLM (Large-Language Model) and get results from an ODP Snowflake database.
+
+This app was adapted from [Frosty](https://quickstarts.snowflake.com/guide/frosty_llm_chatbot_on_streamlit_snowflake).
+
+# Installation
+ 
+You can install the required libraries using:
+```
+pip install requirements.txt
+```
+
+# Configuration
+
+To use the app, you will need to configure the following secrets in your Streamlit app's configuration:
+```
+[connections.snowflake]
+account = "<your_account>"
+user = "<your_user>"
+password = "<your_password>"
+role = "<your_role>"
+warehouse = "<your_warehouse>"
+client_session_keep_alive = true
+
+[azureopenai]
+AZURE_OPENAI_ENDPOINT = "https://<your_endpoint>.openai.azure.com/"
+AZURE_OPENAI_KEY = "<your_key>"
+AZURE_OPENAI_DEPLOYMENT_NAME = "<your_deployment_name>"
+```
+You can set these secrets in a secrets.toml file in a folder called .streamlit (i.e., `.streamlit/secrets.toml`) or in the "Secrets" tab of your Streamlit app's configuration.
+
+Additionally, you need to add the fully qualified table names you want to access in `opti-q.py`. 
+
+# Usage
+
+To run the app, run the following command:
+```
+streamlit run opti-q.py
+```
+
+This will start a Streamlit server and launch the app in your browser.
+
+When the app starts, you will see a dropdown menu that allows you to select the table to query. Once you select a table, you can ask natural language questions to the Azure OpenAI LLM by typing in the chat input box and pressing Enter. The LLM will respond with a natural language response, and if it includes a SQL query, the app will execute the query on the Snowflake table and display the results in a DataFrame.
---
+++ prompts.py
@@ -0,0 +1,91 @@
+"""
+Adapted from Frosty
+https://quickstarts.snowflake.com/guide/frosty_llm_chatbot_on_streamlit_snowflake
+"""
+
+import streamlit as st
+
+QUALIFIED_TABLE_NAME = "PROD_20_WEB.PUBLIC.PRODUCTS"
+TABLE_DESCRIPTION = """
+This table contains information about products.
+"""
+METADATA_QUERY = None
+
+GEN_SQL = """
+You will be acting as an AI Snowflake SQL Expert named Opti-Q.
+Your goal is to give correct, executable sql query to users.
+You will be replying to users who will be confused if you don't respond in the character of Opti-Q.
+You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
+The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
+
+{context}
+
+Here are 6 critical rules for the interaction you must abide:
+<rules>
+1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single snowflake sql code, not multiple. 
+5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
+6. DO NOT put numerical at the very front of sql variable.
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+
+For each question from the user, make sure to include a query in your response.
+
+Now to get started, please briefly introduce yourself, describe the table at a high level, and share the available metrics in 2-3 sentences.
+Then provide 3 example questions using bullet points.
+"""
+
+
+@st.cache_data(show_spinner=False)
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    conn = st.connection("snowflake")
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+                         )
+    columns = "\n".join(
+        [
+            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
+            for i in range(len(columns["COLUMN_NAME"]))
+        ]
+    )
+    context = f"""
+Here is the table name <tableName> {'.'.join(table)} </tableName>
+
+<tableDescription>{table_description}</tableDescription>
+
+Here are the columns of the {'.'.join(table)}
+
+<columns>\n\n{columns}\n\n</columns>
+    """
+    if metadata_query:
+        metadata = conn.query(metadata_query)
+        metadata = "\n".join(
+            [
+                f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
+                for i in range(len(metadata["VARIABLE_NAME"]))
+            ]
+        )
+        context = context + \
+            f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
+    return context
+
+
+def get_system_prompt(table_name: str = QUALIFIED_TABLE_NAME, table_description: str = TABLE_DESCRIPTION):
+    table_context = get_table_context(
+        table_name=table_name,
+        table_description=table_description,
+    )
+    return GEN_SQL.format(context=table_context)
